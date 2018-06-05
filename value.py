09b48b77b700509336fd21e31e4c84deb860ece4
schiflot <39337221+schiflot@users.noreply.github.com>
2018-06-05 15:57:07 +0200

    Add files via upload

--- value.py
+++ value.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import datetime
 from tqdm import tqdm
-import re
+#import regex as re
 
 #import multiprocessing
 
@@ -18,7 +18,7 @@ def assign(res_id, cot_id):
     datum = dfres.get_value(index = res_id-1,col = 'Arrival Date')
     col = dfbez.columns.get_loc(datum)
     duration = dfres.get_value(index = res_id-1,col ='Length of Stay')
-    dfbez.loc[cot_id -1][col:col+duration] += 1
+    dfbez.loc[cot_id -1][col:col+duration] += res_id
     dfres.loc[res_id - 1,'Assigned']=cot_id
     
 def initialize():
@@ -28,7 +28,7 @@ def initialize():
     global dfbez
     global test
 #    file = 'startoplossing.xlsx
-    file = 'startoplossing.xlsx'
+    file = 'startoplossingbb.xlsx'
     
     xl = pd.ExcelFile(file) 
     
@@ -40,7 +40,7 @@ def initialize():
     for i in range(0,42): #Maak lijst van alle beschikbare data als kolom-index
         index.append(startdate + datetime.timedelta(days = i))
     
-    dfbez = pd.DataFrame(np.zeros((820,42), dtype=int),columns = index)
+    dfbez = pd.DataFrame(np.zeros((819,42), dtype=int),columns = index)
     
     for i in tqdm(dfres.index):
         assign(dfres.get_value(index = i,col ='ID'),dfres.get_value(index=i,col='Assigned'))
@@ -55,8 +55,9 @@ weekendgaps = 0
 upgrades = 0
 possible_cap = np.array([2, 4, 5, 6, 8, 12])#mogelijke huizen, uit de handleiding
 
-#dfres = dfres[0:10]
-for i in tqdm(dfbez.index):
+temp= dfbez
+#temp= dfbez[208:209]
+for i in (temp.index):
     test=dfbez.loc[i]
     line = '0'
     for j in test.index:
@@ -67,35 +68,42 @@ for i in tqdm(dfbez.index):
             line = line + str(datetime.date.isoweekday(j)) #1 = maandag ,2 = dinsdag , ... , 7 = zondag
     line = line + '0'
     leg = re.compile('0[1,2,3,4,5,6,7]+0')#werkt niet
-    weekend = re.compile('5671')
+    weekend = re.compile('5671234')
     weekendgaps+=len(weekend.findall(line))
-    g= leg.findall(line)
+#    if len(weekend.findall(line)) != 0:
+#        print(line)
+    g= leg.findall(line,overlapped = True)
+    gaps+=len(g)
     for j in g:
-        gaps+=1
         if len(j) > 23:
             legionella+=1
 
-            
-for k in dfres.index:
+test=dfres[dfres['Cottage (Fixed)']==0]
+#test=test[0:10]
+for k in test.index:
 #        res_in_review = dfres.loc[i]
     assigned = dfres.get_value(index = k,col = 'Assigned')- 1 #corrigeren voor 
 #        cot_in_review = dfcot.loc[assigned]
-    if dfcot.get_value(index = assigned,col = "Class")>dfres.get_value(index = k,col = 'Class'): 
+    if dfcot.get_value(index = assigned,col = "Class") > dfres.get_value(index = k,col = 'Class'): 
         upgrades += 1
-    eff_nr_pers = min(possible_cap[possible_cap > dfres.get_value(index= k,col = "# Persons")])
-    if eff_nr_pers > dfcot.get_value(index = assigned,col = "Max # Pers"): 
-        upgrades += 1
+#        print('Class',dfcot.get_value(index = assigned,col = "Class"),dfres.get_value(index = k,col = 'Class'),k)
+    else:
+        eff_nr_pers = min(possible_cap[possible_cap >= dfres.get_value(index= k,col = "# Persons")])
+        if eff_nr_pers < dfcot.get_value(index = assigned,col = "Max # Pers"): 
+            upgrades += 1
+#            print('Cap',dfres.get_value(index= k,col = "# Persons") , dfcot.get_value(index = assigned,col = "Max # Pers"),k)
 #        res_in_review = dfres.loc[i]
 #        assigned = res_in_review.loc['Assigned']- 1#corrigeren voor 
 #        cot_in_review = dfcot.loc[assigned]
 #        if cot_in_review.loc["Class"]>res_in_review.loc['Class']: upgrades += 1
 #        eff_nr_pers = min(possible_cap[possible_cap > res_in_review.loc["# Persons"]])
 #        if eff_nr_pers > cot_in_review.loc["Max # Pers"]: upgrades += 1
-    
+
+print(upgrades,'upgrades')    
 print(gaps ,'gaps')
 print(weekendgaps,'weekendgaps')
 print(legionella,'legionella')
-print(upgrades,'upgrades')
+print(6*gaps - 3*weekendgaps + 12*legionella + upgrades,'score')
 
 def writer():
     name = input("Naam:")

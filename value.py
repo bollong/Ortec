78ee13efdce4e844178f6a84a5be8cec39a55b3a
bollong <38720257+bollong@users.noreply.github.com>
2018-06-08 21:06:46 +0200

    met beste optie

--- value.py
+++ value.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Mon May 21 15:10:53 2018
-
 @author: frank
 """
 
@@ -11,7 +8,6 @@ import numpy as np
 import datetime
 from tqdm import tqdm
 import regex as re
-
 #import multiprocessing
 
 def assign(res_id, cot_id):
@@ -35,6 +31,7 @@ def is_val_swap(res_id, cot_id):#kijkt of een combinatie reservering/huis geldig
     if dfres.get_value(index=res_id-1,col='# Persons') > dfcot.get_value(index=cot_id-1,col='Max # Pers'):
         return(False) #Huis is te klein
     for opt in options:
+        
         if (dfres.get_value(index=res_id - 1,col =opt) == 1) and (dfcot.get_value(index=cot_id - 1,col =opt) == 0):
             return (False) #Voor alle extra opties, kijk of deze gevraagd en aanwezig zijn
     if dfres.get_value(index=res_id - 1,col='Class') > dfcot.get_value(index=cot_id - 1,col='Class'):
@@ -92,7 +89,7 @@ def initialize():
     global dfbez
     global test
 #    file = 'startoplossing.xlsx
-    file = 'score 3619.xlsx'
+    file = 'score 3442.xlsx'
     
     xl = pd.ExcelFile(file) 
     
@@ -123,81 +120,69 @@ if not 'dfres' in globals():
     initialize()
     
 
-def writer():
-    name = input("Naam:")
-    name = name + ".xlsx"
+def writer(name):
+#    name = input("Naam:")
+#    name = name + ".xlsx"
     
     writer = pd.ExcelWriter(name)
     dfres.to_excel(writer,'Reservations')
     dfbez.to_excel(writer,'Bezetting')
     dfcot.to_excel(writer,'Cottages')
     writer.save()
+
+
+
+
+tussenscore_def = 0
+s=0
+for i in dfcot.index:
+    s+=score(i+1)
+print(s)
+while True:
+    temp=dfres[dfres['Cottage (Fixed)']==0]
+    notzero = pd.unique(dfcot[dfcot['Score']!=0].loc[:]['ID'])
+    temp=temp[temp['ID'].isin(notzero)]
+    for start_res in tqdm(temp.index):
+        start_res+=1
+        cot_id1=dfres.get_value(index=start_res-1,col='Assigned')
+        current=dfbez.loc[cot_id1-1]
+        t=current[current==start_res].index
+        duration = dfres.get_value(index=start_res-1,col='Length of Stay')
+    #    startdate=t[0]
+    #    enddate=t[-1]
+    #    print(startdate,enddate)
+        improved=False
     
-
-temp=dfres[dfres['Cottage (Fixed)']==0]
-notzero = pd.unique(dfcot[dfcot['Score']!=0].loc[:]['ID'])
-temp=temp[temp['ID'].isin(notzero)]
-s=0
-for i in dfcot.index:
-    s+=score(i+1)
-print(s)
-
-for start_res in tqdm(temp.index):
-    start_res+=1
-    cot_id1=dfres.get_value(index=start_res-1,col='Assigned')
-    current=dfbez.loc[cot_id1-1]
-    t=current[current==start_res].index
-    duration = dfres.get_value(index=start_res-1,col='Length of Stay')
-#    startdate=t[0]
-#    enddate=t[-1]
-#    print(startdate,enddate)
-    improved=False
-
-    for cot_id2 in dfcot.index:
-        if not improved:#mogelijk verbeteren
-            cot_id2+=1#Vervang misschien door lookup zodra er gesorteerd wordt
-            if max((dfbez.loc[cot_id2-1][t]))==0:
-                s1=score(cot_id1)+score(cot_id2)
-                if is_val_swap(start_res,cot_id2):
-                    swap(start_res,cot_id1,0,cot_id2)
-                    s2=score(cot_id1)+score(cot_id2)
-                    if s2>s1:
+        for cot_id2 in dfcot.index:
+            if not improved:#mogelijk verbeteren
+                cot_id2+=1#Vervang misschien door lookup zodra er gesorteerd wordt
+                if max((dfbez.loc[cot_id2-1][t]))==0:
+                    s1=score(cot_id1)+score(cot_id2)
+                    if is_val_swap(start_res,cot_id2):
+                        swap(start_res,cot_id1,0,cot_id2)
+                        s2=score(cot_id1)+score(cot_id2)
+                        tussenscore = s1 - s2
+                        #print(tussenscore)
+                        if tussenscore > tussenscore_def:
+                            tussenscore_def = tussenscore
+                            wissel = cot_id2                      
                         swap(start_res,cot_id2,0,cot_id1)
-#                        print('swapping back', start_res,cot_id2)
-                    else:
-                        improved = True
-                        dfres.set_value(start_res-1,'Assigned',cot_id2)
-#                        print('Assuming', start_res,cot_id2)
+        #print(tussenscore_def)
+        #print(cot_id1, 'en', wissel)
+        if tussenscore_def > 0:
+            swap(start_res,cot_id1,0,wissel)
+        tussenscore_def = 0
+        
+                        #if s2>s1:
+                         #   swap(start_res,cot_id2,0,cot_id1)
+    #                        print('swapping back', start_res,cot_id2)
+                        #else:
+                         #   improved = True
+                          #  dfres.set_value(start_res-1,'Assigned',cot_id2)
+    #                        print('Assuming', start_res,cot_id2)
 
-s=0
-for i in dfcot.index:
-    s+=score(i+1)
-print(s)
-
-#for x in pd.unique(dfres['Arrival Date']):
-#    temp=temp[temp['Arrival Date'] == x]
-#    for y in  pd.unique(temp['Length of Stay']):
-#        temp=temp[temp['Length of Stay']==y]
-#        for i in tqdm(temp.index):
-#            cot_id1=temp.get_value(index=i,col='Assigned')
-#            for j in temp.index:
-#                cot_id2=temp.get_value(index=j,col='Assigned')
-#                if i<j:
-#                    s1=score(cot_id1)+score(cot_id2)
-#                    if is_val_swap(i+1,cot_id2) and is_val_swap(j+1,cot_id1):
-#                        swap(i+1,cot_id1,j+1,cot_id2)
-#                        s2=score(cot_id1)+score(cot_id2)
-#                        if s2>s1:
-#                            swap(i+1,cot_id1,j+1,cot_id2)
-#        s=0
-#        for t in dfcot.index:
-#            s+=score(t)
-#        print(s,x,y)
-#                else:
-#                    print('verwissel',i+1,j+1)
-                    
-#cot = 1
-#x=pd.unique(dfbez.loc[0])[0]
-#duration = dfres.get_value(index=x-1,col='Length of Stay')
-#col = dfbez.columns.get_loc(dfres.get_value(index=x-1,col='Arrival Date'))
-#dfbez.loc[cot-1][col:col+duration]=0
+    s=0
+    for i in dfcot.index:
+        s+=score(i+1)
+    print(s)
+    writer('Score' + s)
